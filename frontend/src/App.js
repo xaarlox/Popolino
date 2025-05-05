b4f6706f678fb73438808293b7975a492b69d10e
Valeriia Fedorenko <xaarlox@gmail.com>
2025-05-05 23:34:30 +0300

    feat: add to cart functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
-import { Container } from 'react-bootstrap'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import HomeScreen from './screens/HomeScreen'
-import ProductScreen from './screens/ProductScreen'
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
 
 function App() {
   return (
     <Router>
-      <Header/>
-      <main className='py-3'>
+      <Header />
+      <main className="py-3">
         <Container>
           <Routes>
-            <Route path='/' element={<HomeScreen />} />
-            <Route path='/product/:id' element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+            <Route path="/cart/:productId?" element={<CartScreen />} />
           </Routes>
         </Container>
       </main>
-      <Footer/>
+      <Footer />
     </Router>
   );
 }
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import {
   Row,
   Col,
@@ -9,6 +9,7 @@ import {
   Button,
   Card,
   Container,
+  Form,
 } from "react-bootstrap";
 import Rating from "../components/Rating";
 import Loader from "../components/Loader";
@@ -17,7 +18,10 @@ import "./ProductScreen.css";
 import { listProductDetails } from "../actions/productActions";
 
 function ProductScreen() {
+  const [qty, setQty] = useState(1);
+
   const { id } = useParams();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const productDetails = useSelector((state) => state.productDetails);
   const { loading, error, product } = productDetails;
@@ -25,6 +29,10 @@ function ProductScreen() {
   useEffect(() => {
     dispatch(listProductDetails(id));
   }, [dispatch, id]);
+
+  const addToCartHandler = () => {
+    navigate(`/cart/${id}?qty=${qty}`);
+  };
 
   return (
     <Container>
@@ -83,8 +91,34 @@ function ProductScreen() {
                       </Col>
                     </Row>
                   </ListGroup.Item>
+
+                  {product.countInStock > 0 && (
+                    <ListGroup.Item>
+                      <Row className="d-flex align-items-center">
+                        <Col>Кількість:</Col>
+                        <Col md={6}>
+                          <Form.Control
+                            as="select"
+                            className="py-0"
+                            style={{ width: "45px" }}
+                            value={qty}
+                            onChange={(e) => setQty(e.target.value)}
+                          >
+                            {[...Array(product.countInStock).keys()].map(
+                              (x) => (
+                                <option key={x + 1} value={x + 1}>
+                                  {x + 1}
+                                </option>
+                              )
+                            )}
+                          </Form.Control>
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  )}
                   <ListGroup.Item className="text-center">
                     <Button
+                      onClick={addToCartHandler}
                       className="btn-block"
                       disabled={product.countInStock === 0}
                       type="button"
