fde658c59f9e6ea7da6172aeb263ec0b39bfa643
Valeriia Fedorenko <xaarlox@gmail.com>
2025-05-06 02:18:17 +0300

    feat: cart screen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -10,7 +10,7 @@ import {
   Button,
   Card,
 } from "react-bootstrap";
-import { Message } from "../components/Message";
+import Message from "../components/Message";
 import { addToCart } from "../actions/cartActions";
 
 function CartScreen() {
@@ -23,7 +23,6 @@ function CartScreen() {
 
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
-  console.log("cartItems: ", cartItems);
 
   useEffect(() => {
     if (productId) {
@@ -31,7 +30,98 @@ function CartScreen() {
     }
   }, [dispatch, productId, qty]);
 
-  return <div>Cart</div>;
+  const removeFromCartHandler = (id) => {
+    console.log("remove: ", id);
+  };
+
+  const checkoutHandler = () => {
+    navigate("/login?redirect=shipping");
+  };
+
+  return (
+    <Row>
+      <Col md={8}>
+        <h1>Кошик покупок</h1>
+        {cartItems.length === 0 ? (
+          <Message variant="info">
+            Ваш кошик пустий. <Link to="/">Повернутися назад</Link>
+          </Message>
+        ) : (
+          <ListGroup variant="flush">
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>${item.price}</Col>
+                  <Col md={3}>
+                    <Form.Control
+                      as="select"
+                      className="py-0"
+                      style={{ width: "45px" }}
+                      value={item.qty}
+                      onChange={(e) =>
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value))
+                        )
+                      }
+                    >
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                  <Col md={1}>
+                    <Button
+                      type="button"
+                      variant="light"
+                      onClick={() => removeFromCartHandler(item.product)}
+                    >
+                      <i className="fas fa-trash"></i>
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2>
+                Проміжний підсумок (
+                {cartItems.reduce((acc, item) => acc + item.qty, 0)})
+              </h2>
+              $
+              {cartItems
+                .reduce((acc, item) => acc + item.qty * item.price, 0)
+                .toFixed(2)}
+            </ListGroup.Item>
+          </ListGroup>
+          <ListGroup.Item>
+            <div className="d-flex justify-content-center mt-2 mb-2">
+              <Button
+                type="button"
+                className="px-4 py-2"
+                disabled={cartItems.length === 0}
+                onClick={checkoutHandler}
+              >
+                Оформити замовлення
+              </Button>
+            </div>
+          </ListGroup.Item>
+        </Card>
+      </Col>
+    </Row>
+  );
 }
 
 export default CartScreen;
