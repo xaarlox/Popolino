c81471d21456952fab914c5a1fdee721f3a792ac
Valeriia Fedorenko <xaarlox@gmail.com>
2025-05-06 15:54:38 +0300

    feat: protected routes

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -4,7 +4,10 @@ from . import views
 urlpatterns = [
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', views.getRoutes, name="routes"),
+    
     path('products/', views.getProducts, name="products"),
     path('products/<str:pk>/', views.getProduct, name="product"),
+    
     path('users/profile/', views.getUserProfile, name="users-profile"),
+    path('users/', views.getUsers, name="users"),
 ]
