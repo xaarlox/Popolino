d035231d8e17bf9a9ab9b5b8552376cc1c2d1e20
Valeriia Fedorenko <xaarlox@gmail.com>
2025-05-07 12:07:49 +0300

    feat: profile screen & get user details

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,7 +5,11 @@ import {
   productDetailsReducer,
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
-import { userLoginReducer, userRegisterReducer } from "./reducers/userReducers";
+import {
+  userLoginReducer,
+  userRegisterReducer,
+  userDetailsReducer,
+} from "./reducers/userReducers";
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -13,6 +17,7 @@ const reducer = combineReducers({
   cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
