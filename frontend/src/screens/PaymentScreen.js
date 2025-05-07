922c0a3579391cedc02b735abe42783a9c21a435
Valeriia Fedorenko <xaarlox@gmail.com>
2025-05-07 14:09:59 +0300

    feat: payment screen & save payment method

---
+++ frontend/src/screens/PaymentScreen.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Form, Button, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import FormContainer from "../components/FormContainer";
+import CheckoutSteps from "../components/CheckoutSteps";
+import { savePaymentMethod } from "../actions/cartActions";
+
+function PaymentScreen() {
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [paymentMethod, setPaymentMethod] = useState("PayPal");
+
+  if (!shippingAddress.address) {
+    navigate("/shipping");
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(savePaymentMethod(paymentMethod));
+    navigate("/placeorder");
+  };
+
+  return (
+    <FormContainer>
+      <CheckoutSteps step1 step2 step3 />
+      <Form onSubmit={submitHandler}>
+        <Form.Group>
+          <Form.Label as="legend">Виберіть метод оплати</Form.Label>
+          <Col>
+            <Form.Check
+              type="radio"
+              label="PayPal або кредитна картка"
+              id="paypal"
+              name="paymentMethod"
+              checked
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></Form.Check>
+          </Col>
+        </Form.Group>
+        <Button type="submit" variant="primary" className="mt-3">
+          Продовжити
+        </Button>
+      </Form>
+    </FormContainer>
+  );
+}
+
+export default PaymentScreen;
