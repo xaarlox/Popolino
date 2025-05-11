6c1e72f6d13193c5a403107291dfd1afdb283ee7
Valeriia Fedorenko <xaarlox@gmail.com>
2025-05-12 00:37:08 +0300

    feat: create review endpoint

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -1,9 +1,10 @@
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from base.products import products
-from base.models import Product
+from base.models import Product, Review
 from base.serializers import ProductSerializer
+from rest_framework import status
 
 
 @api_view(['GET'])
@@ -69,3 +70,44 @@ def uploadImage(request):
     product.image = request.FILES.get('image')
     product.save()
     return Response("Фотографію успішно завантажено.")
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def createProductReview(request, pk):
+    user = request.user
+    product = Product.objects.get(_id=pk)
+    data = request.data
+
+    # 1 - Review already exists
+    alreadyExists = product.review_set.filter(user=user).exists()
+
+    if alreadyExists:
+        content = {'details': 'Товар вже оцінено.'}
+        return Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+    # 2 - No rating or 0
+    elif data['rating'] == 0:
+        content = {'details': 'Будь ласка, оберіть рейтинг.'}
+        return Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+    # 3 - Create review
+    else:
+        review = Review.objects.create(
+            user = user,
+            product = product,
+            name = user.first_name,
+            rating = data['rating'],
+            comment = data['comment']
+        )
+
+        reviews = product.review_set.all()
+        product.numReviews = len(reviews)
+
+        total = 0
+        for i in reviews:
+            total += i.rating
+            
+        product.rating = total / len(reviews)
+        product.save()
+        return Response('Відгук додано.')
